e830740c559fe4d6dfd978eb00b386bd574b545c
xornotor <andre.pcr@hotmail.com>
2021-05-05 13:33:54 -0300

    Exercício 2 - Aula 3

---
+++ 04 - Exercícios Aula 3/Exercicio02.asm
@@ -0,0 +1,35 @@
+;André Paiva
+
+%include "io.inc"
+
+section .text
+
+global CMAIN
+CMAIN:
+    mov     ebp,    esp
+    xor     eax,    eax
+    xor     ebx,    ebx
+    xor     ecx,    ecx
+    xor     edx,    edx
+    
+    ;inicialização de eax, ebc e ecx
+    mov     eax,    7FFFFFF8h
+    mov     ebx,    0
+    mov     ecx,    7FFFFFFCh
+    
+WHILE:
+    inc     eax
+    cmp     eax,    ecx
+    je      EDX10
+    mov     edx,    20
+    jmp     CMPAXBX
+    
+    
+EDX10:
+    mov     edx,    10
+    jmp     CMPAXBX
+    
+CMPAXBX:
+    cmp     eax,    ebx
+    jg      WHILE
+    ret
