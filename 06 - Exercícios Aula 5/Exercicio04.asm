4da8e2b777889a44cda1964bb4d4866131f5e688
xornotor <andre.pcr@hotmail.com>
2021-06-05 15:05:03 -0300

    Exercícios 4 e 5, Aula 5

--- 06 - Exercícios Aula 5/Exercicio04.asm
+++ 06 - Exercícios Aula 5/Exercicio04.asm
@@ -1,35 +1,27 @@
 %include "io.inc"
 
-section .bss
-    array   resd            100
+segment .data
+    block1  db      "String1"
+    block2  db      "Stling1"
 
 section .text
 global CMAIN
 CMAIN:
-    mov     ebp,            esp
-    xor     eax,            eax
-    xor     ebx,            ebx
-    xor     ecx,            ecx
-    xor     edx,            edx
+    mov     ebp,    esp
+    xor     ecx,    ecx
+    xor     edx,    edx
     
     cld
-    
-    mov     edi,            array
-    mov     ecx,            100
-    mov     eax,            12
-
-lp:
-    mov     [array+ebx],    edx
-    add     ebx,            4
-    inc     edx
-    scasd
-    je      found
-    loop    lp
-    
-    jmp     onward
-
-found:
-    sub     edi,    4
+    mov     esi,    block1
+    mov     edi,    block2
+    mov     ecx,    7
+    repe    cmpsb
+    jne     onward
+    mov     edx,    1
+    jmp     finish
     
 onward:
+    mov     edx,    0
+    
+finish:    
     ret
